dbf183c9e8b512e768e3a4205575ffc0a06bf72e
Marcelo Carvalho <marcelomagal@gmail.com>
2023-11-24 17:29:07 -0300

    Implementa funcionalidades iniciais do back-end, excluindo autenticação
    - Configura servidor básico com Express no diretório backend.
    - Estabelece conexão com o banco de dados PostgreSQL usando Sequelize.
    - Cria banco de dados para teste `gastrotech_test`.

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    development: {
+      username: 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: 'gastrotech_test',
+      host: '127.0.0.1',
+      dialect: 'postgres',
+      // Adicione outras configurações necessárias
+    },
+    // Você pode adicionar mais configurações para outros ambientes
+  };
+  
